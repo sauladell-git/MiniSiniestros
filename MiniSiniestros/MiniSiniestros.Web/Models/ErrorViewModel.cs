d9d752a7f37107f3eb02dc356855cf990a077f96
sauladell <sebaauladell@gmail.com>
2026-08-19 21:18:36 -0300

    config docker / fix  MiniSinistros.Web ( - razor pages + MVC )

---
+++ MiniSiniestros/MiniSiniestros.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniSiniestros.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
